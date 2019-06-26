ce2bbb56d5957f6f336ecd2655d012a8e20bd8bf
Don Choppy <wxtem@hotmail.com>
2019-06-26 14:57:07 -0400

    se crea api list para material

--- src/support_material/views.py
+++ src/support_material/views.py
@@ -1,7 +1,13 @@
+from .models import Material
 from .serializers import MaterialSerializer
 from rest_framework.response import Response
+from rest_framework.generics import ListAPIView
 from rest_framework.generics import CreateAPIView
 # Create your views here.
 
 class MaterialUploadView(CreateAPIView):
 	serializer_class = MaterialSerializer
+
+class ListMaterialView(ListAPIView):
+	serializer_class = MaterialSerializer
+	queryset = Material.objects.filter(active=True)
