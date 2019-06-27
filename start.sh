b41b45a80d02d0006f9492a0cf024ef4e13341ae
Don Choppy <wxtem@hotmail.com>
2019-06-26 22:18:03 -0400

    se agrega collect static a start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,10 @@ echo "Applying migrations"
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 
+# collect static
+echo "Collecting static files"
+python manage.py collectstatic --no-input
+
 # start server
 echo "Starting server"
 python manage.py runserver 0:8080
